06abcf67344ee52e82035cb4b247ed430ffb148f
Magda <m.stebel@mailplus.pl>
2022-05-21 18:32:38 +0200

    Stworzenie projektu

---
+++ src/test/java/pl/ms/householdbudget/HouseholdbudgetApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.ms.householdbudget;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HouseholdbudgetApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
